a79df82cd878aa540f144d331ba9f9581cd8f195
lifaping789 <449117588@qq.com>
2018-01-02 22:01:52 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+//作者:lifaping
+//功能：在控制台显示“Hello”
+//日期：20171114
+//public:表示这个类是公共的，一个JAVA文件中只有一个公共类
+//class:表示这个是一个类
+//Hello：类名(公共类的类名必须和文件名一致)
+public class Hello
+{
+	//一个主函数，相当于程序的入口
+	public static void main(String args[])
+	{
+		System.out.println("Hello,Java!");
+	}
+}
